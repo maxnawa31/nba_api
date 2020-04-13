c04f77b8eddb7bd0dd8f3316b65c4f1f9e0df8d2
maxnawa31 <mnawa31@gmail.com>
2020-04-13 13:10:55 -0700

    small changes

--- db/index.js
+++ db/index.js
@@ -1,8 +1,9 @@
-const { Client } = require("pg");
-const { DB, DB_SECRET, DB_USER, DB_HOST } = process.env;
-
+const { Client } = require('pg');
+const { DB, DB_HOST } = process.env;
+const types = require('pg').types
+types.setTypeParser(1700, 'text', parseFloat)
 client = new Client({
-	connectionString: `postgresql://${DB_USER}:${DB_SECRET}@${DB_HOST}/${DB}`
+  connectionString: `postgresql://${DB_HOST}/${DB}`
 });
 client.connect();
 
--- routes/teams.js
+++ routes/teams.js
@@ -1,35 +1,39 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const db = require("../db");
+const db = require('../db');
 
-router.get("/", async (req, res, next) => {
-	try {
-		const result = await db.query("SELECT * FROM teams;");
-		return res.json(result.rows);
-	} catch (err) {
-		console.log(err);
-	}
+router.get('/', async (req, res, next) => {
+  try {
+    const result = await db.query('SELECT * FROM teams;');
+    return res.json(result.rows);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-router.get("/teams/stats", async (req, res, next) => {
-	const { category, year } = req.query;
-	table_name = `team_stats_${category}_${year}`
-	const result = await db.query(`SELECT * from team_stats_${category}_${year} AS stats JOIN teams ON stats.team_id=teams.id;`)
-	console.log(result.rows.length)
-	return res.json(result.rows)
+router.get('/teams/stats', async (req, res, next) => {
+  const { category, year } = req.query;
+
+  table_name = `team_stats_${category}_${year}`;
+  const result = await db.query(
+    `SELECT * from team_stats_${category}_${year} AS stats JOIN teams ON stats.team_id=teams.id;`
+  );
+
+  return res.json(result.rows);
 });
 
-router.get("/teams/:id", async (req, res, next) => {
-	console.log(req.params);
-	try {
-		const {
-			params: { id }
-		} = req;
-		const result = await db.query(`SELECT * FROM teams WHERE id=${id}`);
-		return res.json(result.rows);
-	} catch (err) {
-		console.log(err);
-	}
+router.get('/teams/:id/:year', async (req, res, next) => {
+  try {
+    const {
+      params: { id, year },
+    } = req;
+    const result = await db.query(
+      `SELECT * from player_stats_per_game_${year} AS stats JOIN players ON stats.player_id = players.id JOIN teams ON teams.id=stats.team_id WHERE team_id=${id}`
+    );
+    return res.json(result.rows);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
